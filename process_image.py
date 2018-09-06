c1c30cc69cffc53ce767c5a976b09c3f0d6821e5
smwitkowski <stephen.m.witkowski@gmail.com>
2018-09-05 22:54:40 -0400

    Documented read_image

--- process_image.py
+++ process_image.py
@@ -1,4 +1,5 @@
 import cv2
+import pytesseract
 
 
 def find_template(image, templates):
@@ -54,49 +55,6 @@ def read_pause_menu(image, score_template, pause_templates, threshold, gray_imag
     else:
         return 0
 
-
-def find_largest_area(contours):
-    area_list = []
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        area = w * h
-        area_list.append(area)
-    area_list_sort = sorted(area_list)
-    x, y, w, h = cv2.boundingRect(area_list_sort[-1])
-    return x, y, w, h
-
-
-def read_name(image):
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (4, 4))
-    dilated = cv2.dilate(image, kernel, iterations=5)
-    img, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = find_largest_area(contours)
-    cropped = image[y:y + h, x:x + w]
-    return cropped
-
-
-def read_image(image, type):
-    thresh = cv2.adaptiveThreshold(image,
-                                   255,
-                                   cv2.ADAPTIVE_THRESH_MEAN_C,
-                                   cv2.THRESH_BINARY_INV,
-                                   31,
-                                   21)
-    cv2.imshow('image', thresh)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    if type.lower() == "name":
-        image_iso = read_name()
-        text = pytesseract.image_to_string(image_iso,
-                                           config="-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ -psm 7 -oem 2 no_dict")
-        return text
-    elif type.lower() == 'score':
-        text = pytesseract.image_to_string(image_iso, config="-c tessedit_char_whitelist=1234567890 -psm 6")
-        return text
-    else:
-        return "Please define the type of picture. It's either a 'name' or 'score'."
-
-
 def find_largest_area(contours):
     # Create an empty list that will be populated later
     area_list = []
@@ -143,3 +101,39 @@ def read_team_and_score(image, gray_image):
     away_team = score_box[40:165, 750:850]
 
     return away_score, away_team, home_score, home_team
+
+def read_name(image):
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (4, 4))
+    dilated = cv2.dilate(image, kernel, iterations=5)
+    img, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = find_largest_area(contours)
+    cropped = image[y:y + h, x:x + w]
+    return cropped
+
+
+def read_image(image, type):
+    # Apply adaptive thresholding to the image
+    # The image 'lighting; will rarely change.
+    # TODO change the adaptiveThreshold to threshold
+    thresh = cv2.adaptiveThreshold(image,
+                                   255,
+                                   cv2.ADAPTIVE_THRESH_MEAN_C,
+                                   cv2.THRESH_BINARY_INV,
+                                   31,
+                                   21)
+
+    # If the image type is the team name, then continue
+    if type.lower() == "name":
+        #
+        image_iso = read_name(thresh)
+        text = pytesseract.image_to_string(image_iso,
+                                           config="-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ -psm 7 -oem 2 no_dict")
+        return text
+    elif type.lower() == 'score':
+        text = pytesseract.image_to_string(image_iso, config="-c tessedit_char_whitelist=1234567890 -psm 6")
+        return text
+    else:
+        return "Please define the type of picture. It's either a 'name' or 'score'."
+
+
+
